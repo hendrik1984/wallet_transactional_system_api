134d671ab0731ba4e32ced4c6344568b7b1fb604
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-03 15:32:05 +0700

    add routes for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, path: 'wallet/users'
+      resources :transactions
       resources :wallets
       resources :transactions
     end
