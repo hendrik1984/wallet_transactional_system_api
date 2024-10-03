197ead97ee83db744516c42a8b72d52b70300677
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-03 11:03:40 +0700

    add routes users with modify path into wallet/users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      resources :users, path: 'wallet/users'
       resources :wallets
       resources :transactions
     end
