b4c59cd89c30873c8659e411f78dd22a7c1ce6aa
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-04 15:29:12 +0700

    add method transfer and rafactor code

--- app/controllers/api/v1/wallets_controller.rb
+++ app/controllers/api/v1/wallets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::WalletsController < ApplicationController
-  before_action :set_wallet, only: %i[show deposit withdraw]
+  before_action :set_wallet, only: %i[show deposit withdraw transfer]
   
   def index
     @wallets = Wallet.all
@@ -13,7 +13,6 @@ class Api::V1::WalletsController < ApplicationController
   def deposit
     begin
       ActiveRecord::Base.transaction do
-        @wallet = Wallet.find(params[:id])
         @wallet.deposit(params[:amount], params[:message])
         render json: JsonCustomResponse.reformat(@wallet.transactions.last, "", 201), status: :created
       end
@@ -25,8 +24,29 @@ class Api::V1::WalletsController < ApplicationController
   def withdraw
     begin
       ActiveRecord::Base.transaction do
-        @wallet = Wallet.find(params[:id])
         @wallet.withdraw(params[:amount], params[:message])
+        render json: JsonCustomResponse.reformat(@wallet.transactions.last, "", 201), status: :created
+      end
+    rescue => e
+      render json: JsonCustomResponse.reformat("", e.message, 422), status: :unprocessable_entity
+    end
+  end
+
+  def transfer
+    begin
+      ActiveRecord::Base.transaction do
+        @target_wallet = Wallet.find(params[:target_wallet_id])
+
+        # check account
+        if @wallet.id == @target_wallet.id
+          raise 'Please check account destination no!'
+        end
+
+        # self Wallet
+        @wallet.withdraw(params[:amount], "You have sent amount to wallet id: #{@target_wallet.id} name: #{@target_wallet.name}")
+
+        # target Wallet
+        @target_wallet.deposit(params[:amount], params[:message])
         render json: JsonCustomResponse.reformat(@wallet.transactions.last, "", 201), status: :created
       end
     rescue => e
