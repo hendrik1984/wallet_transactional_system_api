700f64cacbf42a1840dc8d2f81db52eb7187cf5f
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-04 14:53:02 +0700

    add methods deposit and withdraw on wallet controller

--- app/controllers/api/v1/wallets_controller.rb
+++ app/controllers/api/v1/wallets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::WalletsController < ApplicationController
-  before_action :set_wallet, only: %i[show]
+  before_action :set_wallet, only: %i[show deposit withdraw]
   
   def index
     @wallets = Wallet.all
@@ -8,6 +8,30 @@ class Api::V1::WalletsController < ApplicationController
 
   def show
     render json: JsonCustomResponse.reformat(@wallet, '', 200), status: :ok
+  end
+
+  def deposit
+    begin
+      ActiveRecord::Base.transaction do
+        @wallet = Wallet.find(params[:id])
+        @wallet.deposit(params[:amount], params[:message])
+        render json: JsonCustomResponse.reformat(@wallet.transactions.last, "", 201), status: :created
+      end
+    rescue => e
+      render json: JsonCustomResponse.reformat("", e.message, 422), status: :unprocessable_entity
+    end
+  end
+
+  def withdraw
+    begin
+      ActiveRecord::Base.transaction do
+        @wallet = Wallet.find(params[:id])
+        @wallet.withdraw(params[:amount], params[:message])
+        render json: JsonCustomResponse.reformat(@wallet.transactions.last, "", 201), status: :created
+      end
+    rescue => e
+      render json: JsonCustomResponse.reformat("", e.message, 422), status: :unprocessable_entity
+    end
   end
 
   private
