ef38d5e50c02dc01b54024706efc0c8d175b1847
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-05 18:20:28 +0700

    create new library module for latest stock price

---
+++ lib/latest_stock_price.rb
@@ -0,0 +1,54 @@
+require 'uri'
+require 'net/http'
+
+module LatestStockPrice
+  class Client
+    def initialize(api_key)
+      @api_key = api_key
+      @base_url = 'https://latest-stock-price.p.rapidapi.com'
+      @host = "latest-stock-price.p.rapidapi.com"
+    end
+
+    def price(symbol)
+      url = URI("#{@base_url}/equities-search")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request["x-rapidapi-key"] = @api_key
+      request["x-rapidapi-host"] = @host
+
+      response = http.request(request)
+      return response.read_body
+    end
+
+    def prices(symbols)
+      url = URI("#{@base_url}/equities-enhanced")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request["x-rapidapi-key"] = @api_key
+      request["x-rapidapi-host"] = @host
+
+      response = http.request(request)
+      return response.read_body
+    end
+
+    def price_all
+      url = URI("#{@base_url}/equities")
+      
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      
+      request = Net::HTTP::Get.new(url)
+      request["x-rapidapi-key"] = @api_key
+      request["x-rapidapi-host"] = @host
+      
+      response = http.request(request)
+      return response.read_body
+    end
+  end
+end
